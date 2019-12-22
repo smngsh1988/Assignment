2ef5a3178375ec8bd9cc903a7690cafe9ecbb194
suman.ghosh <suman.ghosh@L505283>
2019-12-23 01:31:56 +0530

    Pushed assignment code test 1

--- AssignmentTrial/Controllers/DefaultController.cs
+++ AssignmentTrial/Controllers/DefaultController.cs
@@ -1,7 +1,9 @@
 ﻿using AssignmentTrial.Models;
 using Newtonsoft.Json;
+using System;
 using System.IO;
 using System.Net;
+using System.Web;
 using System.Web.Mvc;
 
 namespace AssignmentTrial.Controllers
@@ -11,14 +13,44 @@ namespace AssignmentTrial.Controllers
         // GET: Default
         public ActionResult Index()
         {
+            string path = Convert.ToString(TempData["PATH"]);
             WebClient WC = new WebClient();
-            var JSON = WC.DownloadString(Path.Combine(Server.MapPath("~/bin"), "raw_data.json"));
+            var JSON = WC.DownloadString(path);//Path.Combine(Server.MapPath("~/bin"), "raw_data.json"));
             TestInputCollection testInputCollection = JsonConvert.DeserializeObject<TestInputCollection>(JSON);
 
             var validationResult = testInputCollection.ValidateCollection();
 
 
             return View(validationResult);
+        }
+
+        public ActionResult UploadFile()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        public ActionResult UploadFile(HttpPostedFileBase file)
+        {
+            try
+            {
+                string _path = string.Empty;
+                if (file.ContentLength > 0)
+                {
+                    string _FileName = Path.GetFileName(file.FileName);
+                    _path = Path.Combine(Server.MapPath("~/UploadedFiles"), _FileName);
+                    file.SaveAs(_path);
+                }
+                ViewBag.Message = "File Uploaded Successfully!!";
+                TempData["PATH"] = _path;
+                return RedirectToAction("Index");
+            }
+            catch (Exception e)
+            {
+                ViewBag.Message = "File upload failed!!";
+                return View();
+            }
         }
     }
 }
